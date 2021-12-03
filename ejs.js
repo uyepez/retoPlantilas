3038c6b7f02a4bc758e9ffa2096eead5e7dd3f11
uyepez <uyepez@tvp.mx>
2021-12-03 09:22:49 -0600

    Initial commit

---
+++ ejs.js
@@ -0,0 +1,63 @@
+var express = require('express')
+var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set('views','./views/views_ejs')
+app.set('view engine', 'ejs');
+
+
+const listaProductos = []
+
+const listaImagenes = [
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/019_-_Star-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/006_-_Present-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/025_-_Mince_Pie-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/018_-_Candle-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/007_-_Present-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/58/015_-_Stocking-128.png"},
+    { foto: "https://cdn1.iconfinder.com/data/icons/christmas-flat-4/59/026_-_Baubles-128.png"}
+]
+
+
+//index vista de formulario
+app.get('/', function(req, res){
+    const randomElement = listaImagenes[Math.floor(Math.random() * listaImagenes.length)];
+    res.render('layouts/index',{
+        foto: randomElement.foto,
+        mensaje:''
+    })
+})
+
+
+//vista de lista
+app.post('/productos', function (req, res) {
+    console.log("body",req.body);
+    let producto = { 
+        title: req.body.title,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    }
+    listaProductos.push(producto)
+    console.log(listaProductos);
+    const randomElement = listaImagenes[Math.floor(Math.random() * listaImagenes.length)];
+
+    res.render('layouts/index', {
+        foto: randomElement.foto,
+        mensaje: "Producto almacenado correctamente."
+    })
+})
+
+app.get('/lista', function (req, res) {
+    //listaProductos=[]
+
+    res.render('layouts/lista', {
+        lista: listaProductos,
+        totalProductos: listaProductos.length
+    })
+})
+
+//listen 
+app.listen(3000)
+console.log('3000 es mi puerto');
